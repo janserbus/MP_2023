2daf8d99e131a79da54c27b161c73969e3c2bbd6
Jan Serbus <jserbus@seznam.cz>
2023-02-11 18:34:05 +0100

    header done

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,7 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
+const dropdown = document.querySelector(".menu__list--dropdown");
 
 hamburger.addEventListener("click", () =>{
     hamburger.classList.toggle("active");
-    menu.classList.toggle("active");
+    menu.classList.toggle("active");    
+});
+
+dropdown.addEventListener("click", () =>{
+    dropdown.classList.toggle("open");
 });
