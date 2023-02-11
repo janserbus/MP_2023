93a9037af3e35cb5bf731e4c2c1f5f9c24dc2ae2
Jan Serbus <jserbus@seznam.cz>
2023-02-11 21:43:32 +0100

    added functional slider

---
+++ scripts/slider.js
@@ -0,0 +1,15 @@
+var slider = tns({
+    container: '.slider',
+    items: 3,
+    slideBy: '1',
+    speed: 700,
+    gutter: 20,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    rewind: true,
+    controls: ".slider__controls",
+    prevButton: ".controls__prev",
+    nextButton: ".controls__next",
+    navPosition: "bottom",
+    autoplayButtonOutput: false
+  });
